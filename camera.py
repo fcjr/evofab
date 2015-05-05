4807d5a7755dda149636d4e15a25f65d7687d0a1
John Peterson <sn0risn@gmail.com>
2015-05-04 22:50:44 -0400

    ok it actually works now. probably. for realsies

--- camera.py
+++ camera.py
@@ -17,9 +17,6 @@ class Camera:
 
         in_view = []
 
-        #for cell in [(i, j) for i in range(size) for j in range(size)]: this
-        #doesn't belong here -- would be used to do this for all cells in camera
-
         cam_cell_x, cam_cell_y = self.get_pixel_coords(camera_cell)
 
         top_left_diff = (cam_cell_x % self.width, cam_cell_y % self.width)
@@ -35,13 +32,17 @@ class Camera:
                 in_view.append(self.get_cell_coords((x(top_left) + self.width, y(top_left))))
         elif (y(top_left_diff) != 0):
                 in_view.append(self.get_cell_coords((x(top_left), y(top_left) + self.width)))
+
+        #ignore cells that would be off the build platform
+        in_view = [(a, b) for a,b in in_view if (a >= 0 and b >= 0) and (a < self.size * self.width and b < self.size * self.width)]
         return in_view
 
     def get_pixel_coords(self, cell_coord):
         upper_center_corner = (self.printer.position.x - self.width/2.0, self.printer.position.y - self.width/2.0)
         assert isinstance(self.size, int) == True #to keep the next line from not doing what it's supposed to do
-        upper_camera_corner = (x(upper_center_corner) - self.size/2, y(upper_center_corner) - self.size/2)
-        pixel_coords = (x(upper_camera_corner) + (x(cell_coord) * self.width), y(upper_camera_corner) + (y(cell_coord * self.width)))
+        #upper_camera_corner = (x(upper_center_corner) - self.size/2, y(upper_center_corner) - self.size/2) #line below fixes this broken line
+        upper_camera_corner = (x(upper_center_corner) - self.width, y(upper_center_corner) - self.width) #TODO this assumes that camera is size 3
+        pixel_coords = (x(upper_camera_corner) + (x(cell_coord) * self.width), y(upper_camera_corner) + (y(cell_coord) * self.width))
         return pixel_coords
 
     def get_cell_coords(self, pixel_coord):
--- camera_tester.py
+++ camera_tester.py
@@ -5,6 +5,6 @@ import pygame
 
 #camera tester
 the_grid = GridWorld(10, 10, 10)
-printer = VirtualPrinter(15, 10, 1, 0, pygame.color.Color("darkorange"), the_grid)
+printer = VirtualPrinter(0, 0, 1, 0, pygame.color.Color("darkorange"), the_grid)
 camera = Camera(printer, the_grid.grid, 10)
-print camera.get_cells_in_view((0, 0))
+print camera.get_cells_in_view((1, 1))
