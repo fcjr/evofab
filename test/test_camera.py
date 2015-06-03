b90c531a43107b0c28fc51626c4bb6ea3cf3837d
John Peterson <sn0risn@gmail.com>
2015-06-02 20:27:11 -0400

    even works on arbitrary camera size

--- test/test_camera.py
+++ test/test_camera.py
@@ -82,3 +82,12 @@ class TestCamera(unittest.TestCase):
         self.grid.set_loc_val(1, 2, 0)
         self.grid.set_loc_val(2, 2, 0)
         self.assertEqual(self.camera.percent_in_view(Vector(1, 1)), 0.25)
+
+    def test_region_over_four_cells_with_arbitrary_camera_size(self):
+        localcamera = Camera(self.grid, self.printer, 5)
+        self.printer.position = Vector(20, 20)
+        self.grid.set_loc_val(1, 1, 1)
+        self.grid.set_loc_val(2, 1, 0)
+        self.grid.set_loc_val(1, 2, 0)
+        self.grid.set_loc_val(2, 2, 0)
+        self.assertEqual(self.camera.percent_in_view(Vector(1, 1)), 0.25)
