8abb14fa1bc84076b2c7ccbf1ab266cff3f5ec65
John Peterson <sn0risn@gmail.com>
2015-06-19 15:57:54 -0400

    adds importing grids

--- src/grid.py
+++ src/grid.py
@@ -1,10 +1,31 @@
+def load_grid_from_file(path):
+    grid = []
+    with open(path, 'r') as to_read:
+        for line in to_read:
+            grid.append([])
+            for char in line.strip():
+                grid[-1].append(int(char))
+    return grid
+
 class Grid:
     """Operates as a grid with values"""
 
     empty = 0
     filled = 1
 
-    def __init__(self,wid,hi,scale):
+    def __init__(self,wid=None,hi=None,scale=None, path=None):
+        """can either be constructed with a width, height, and scale (init to empty)
+        or with a scale and a path"""
+        if not scale:
+            #you have to provide a scale and if you don't this should break things
+            assert(1 == 2) #TODO: do this with an exception
+        if path:
+            self.grid = load_grid_from_file(path)
+            grid = self.grid
+            self.height = len(grid)
+            self.width = len(grid[0])
+            self.gridsize = scale
+        else:
             self.width = wid
             self.height = hi
             self.gridsize = scale
@@ -23,6 +44,7 @@ class Grid:
         (x,y) = p
         return (x >= 0) and (x < self.width*self.gridsize) and (y >= 0) and (y < self.height*self.gridsize)
 
+    #TODO: this shouldn't be here. should be in the visual grid. same with `scale` variable
     def find_closest_gridloc(self,p):
             '''given a point in (high resolution) game space, return the closest grid point'''
             x,y = p
--- test/test_grid.py
+++ test/test_grid.py
@@ -5,6 +5,10 @@ class GridTests(unittest.TestCase):
     def setUp(self):
         self.grid = Grid(10, 10, 5)
 
+    def test_init_from_file(self):
+        grid = Grid(scale=7, path='test/resources/example')
+        self.assertEqual(grid.grid, [ [0,1,0,1,0,1,0,1,0,1], [1,0,1,0,1,0,1,0,1,0], [1,1,1,1,1,1,1,1,1,1], [0,1,0,1,0,1,0,1,0,1] ])
+
     def test_value_changes(self):
         self.grid.set_loc_val(3, 3, 1)
         assert self.grid.val_at(3, 3) == 1
