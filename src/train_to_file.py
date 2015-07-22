43ac0cf235e485ba3fa9a2fea5b9bb332cf6bb3e
John Peterson <sn0risn@gmail.com>
2015-07-22 15:46:16 -0400

    training pulls from a directory instead of individual files

--- src/train_to_file.py
+++ src/train_to_file.py
@@ -2,12 +2,16 @@ from ann import Network
 from ann_trainer import AnnTrainer
 import ann_io
 import sys
+from os import listdir
+from os.path import isfile, join
 
 ann_extension = '.ann'
 
 outputfile = sys.argv[1]
 iterations = int(sys.argv[2])
-files = sys.argv[3:]
+training_dir = sys.argv[3]
+
+files = [ join(training_dir, f) for f in listdir(training_dir) if isfile(join(training_dir, f)) ]
 
 n = Network(9, 7, 4)
 trainer = AnnTrainer()
