846af28af10b4a786b5f7f644217f045c9830284
John Peterson <sn0risn@gmail.com>
2015-07-28 19:06:36 -0400

    ready to run a job on jupiter

--- src/ga_runner.py
+++ src/ga_runner.py
@@ -8,5 +8,5 @@ run_type = sys.argv[2]
 is_visual = mode == 'visual'
 is_dumping = run_type == 'exp'
 
-population = AnnPopulation(200, .1, 40, 9, 6, 4, Grid(scale=50, path='corner.test'), is_visual=is_visual, dump_to_files=is_dumping)
-population.iterate(1000, 12)
+population = AnnPopulation(300, .1, 60, 9, 8, 4, Grid(scale=50, path='corner.test'), is_visual=is_visual, dump_to_files=is_dumping)
+population.iterate(2000, 12)
