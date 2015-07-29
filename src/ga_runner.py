818ec0e507f70380c43459c20093e463a67cd930
John Peterson <sn0risn@gmail.com>
2015-07-28 23:25:11 -0400

    fixed -- should have been replacing 80% of pop. not 20%

--- src/ga_runner.py
+++ src/ga_runner.py
@@ -8,5 +8,5 @@ run_type = sys.argv[2]
 is_visual = mode == 'visual'
 is_dumping = run_type == 'exp'
 
-population = AnnPopulation(300, .1, 60, 9, 8, 4, Grid(scale=50, path='corner.test'), is_visual=is_visual, dump_to_files=is_dumping)
+population = AnnPopulation(300, .1, 240, 9, 8, 4, Grid(scale=50, path='corner.test'), is_visual=is_visual, dump_to_files=is_dumping)
 population.iterate(2000, 12)
