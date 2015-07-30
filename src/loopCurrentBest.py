d89c6a74dad302d430cb10c0a31f77a31933e2e1
John Rieffel <rieffelj@Johns-iMac-2.local>
2015-07-29 23:43:04 -0400

    A nifty utility to repeatedly run the best individual graphically

---
+++ src/loopCurrentBest.py
@@ -0,0 +1,14 @@
+from gui_ann_runner import GuiAnnRunner
+from grid import Grid
+import ann_io
+import sys
+
+ann_file = sys.argv[1]
+world_file = sys.argv[2]
+
+
+while(True):
+	n = ann_io.load(ann_file)
+	ideal_grid = Grid(scale=50, path=world_file)
+	runner = GuiAnnRunner(ideal_grid)
+	runner.run(n, iterations=1000, x=10, y=4)
