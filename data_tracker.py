93ace9b9281ccf2031dc40e31ef00e2dbbe0a749
John Peterson <sn0risn@gmail.com>
2015-07-31 11:37:18 -0400

    real time data plotter (can work remotely!)

--- data_tracker.py
+++ data_tracker.py
@@ -1,23 +1,50 @@
 import matplotlib.pyplot as plt
-import time
 import csv
+import os
+import sys
+
+
+tmpdir_name = 'tracker_tmp/'
 
 last_gen_num = -1
 maxs = []
 
+remote = False
+remote_dir = None
+if sys.argv[1] == '-r':
+    remote_dir = sys.argv[2]
+    remote = True
+
+def get_file():
+    print 'getting'
+    if remote:
+        assert remote_dir != None
+        os.system('rsync -avzhe ssh petersoj@jupiter.union.edu:' + remote_dir + 'data/' + ' ' + tmpdir_name)
+        return tmpdir_name + 'stats.csv'
+    else:
+        return 'data/stats.csv'
+
+def clean():
+    if remote:
+        os.system('rm -rf ' + tmpdir_name)
+
 plt.ion()
-with open('data/stats.csv', 'r') as inputfile:
-    reader = csv.reader(inputfile)
-    reader.next()
-    gen_vals = [map(int, row) for row in reader]
-    maxs = [row[2] for row in gen_vals]
-    last_gen_num = gen_vals[-1][0]
-    plt.plot(range(last_gen_num + 1), maxs)
-    plt.axis([0, last_gen_num + 1, 0, 5 * max(maxs)])
-    plt.draw()
-    plt.pause(0.05)
+gen_vals = []
+while len(gen_vals) < 1:
+    with open(get_file(), 'r') as inputfile:
+        reader = csv.reader(inputfile)
+        reader1 = [x for x in reader][1:]
+        print reader1
+        gen_vals = [map(int, row) for row in reader1]
+    clean()
+maxs = [row[2] for row in gen_vals]
+last_gen_num = gen_vals[-1][0]
+plt.plot(range(last_gen_num + 1), maxs)
+plt.axis([0, last_gen_num + 1, 0, 5 * max(maxs)])
+plt.draw()
+plt.pause(0.05)
 while True:
-    with open('data/stats.csv', 'r') as inputfile:
+    with open(get_file(), 'r') as inputfile:
         reader = csv.reader(inputfile)
         reader.next()
         gen_vals = [map(int, row) for row in reader]
@@ -28,3 +55,4 @@ while True:
             plt.axis([0, last_gen_num + 1, 0, 5 * max(maxs)])
         plt.draw()
         plt.pause(0.05)
+    clean()
