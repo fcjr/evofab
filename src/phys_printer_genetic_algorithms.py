d29665bdfb7ef5ce46816af5b329c22899fbd40c
John Peterson <sn0risn@gmail.com>
2016-06-23 11:28:37 -0400

    began structure for hardware integration

---
+++ src/phys_printer_genetic_algorithms.py
@@ -0,0 +1,7 @@
+from ann_genetic_algorithms import AnnGenotype
+
+class PhysGenotype(AnnGenotype):
+    def calculate_fitness(self):
+        """ implement hardware-specific exectution and eval
+        code for the EvoFab hardware system"""
+        return
