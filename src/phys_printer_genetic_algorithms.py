22ab3fdef031d208921fb21ceb7f5099c0c4f550
Frank Chiarulli Jr <frank@frankchiarulli.com>
2016-07-20 15:40:54 -0400

    (pete) modifies physical printer code to use conveyor belt

--- src/phys_printer_genetic_algorithms.py
+++ src/phys_printer_genetic_algorithms.py
@@ -15,13 +15,14 @@ class PhysPopulation(AnnPopulation):
     execution of the GA within members
     """
 
-    def __init__(self, random_seed, printer_runtime, size, mutation_rate, mutation_range, crossover_rate, replacement_number, num_input, num_hidden, num_output, serial_port, sensor_serial_port, camera, outputfolder, crop=True, is_visual=True, dump_to_files=False):
+    def __init__(self, random_seed, printer_runtime, size, mutation_rate, mutation_range, crossover_rate, replacement_number, num_input, num_hidden, num_output, serial_port, sensor_serial_port, conveyor_port, camera, outputfolder, crop=True, is_visual=True, dump_to_files=False):
         super(PhysPopulation, self).__init__(random_seed, printer_runtime, size, mutation_rate, mutation_range, crossover_rate, replacement_number, num_input, num_hidden, num_output, outputfolder, is_visual=is_visual, dump_to_files=dump_to_files)
         self.genotype_factory = PhysGenotypeFactory(self)
         #TODO: should probably test that sensor and controller serial ports are valid
         self.controller = EvoController(serial_port)
         self.sense = EvoArray(sensor_serial_port)
         self.camera = EvoCamera(camera, crop)
+        self.conveyor = EvoConveyor(conveyor_port)
         listener = threading.Thread(target=kbdListener)
         listener.start()
 
@@ -54,8 +55,11 @@ class PhysGenotype(AnnGenotype):
         print("evaluating...")
         self.population.controller.testHome()
         fitness = self.population.camera.eval()
-        time.sleep(5)
+        time.sleep(2)
         self.fitness = fitness * 100
+        print "fitness:", self.fitness
+        self.population.camera.showImage()
+        self.population.conveyor.run()
 
     def get_velocity(self, instruction):
         """Basic translation from expected outputs from the neural network
@@ -82,6 +86,7 @@ class PhysGenotype(AnnGenotype):
         c.home()
         start_time = time.time()
         c.extrude()
+        time.sleep(4)
         while time.time() - start_time < self.population.printer_runtime:
             #if kbdInput == "q":
             #    c.pause()
@@ -91,6 +96,7 @@ class PhysGenotype(AnnGenotype):
             #run the printer based on neural net responses
             photo_array_values = self.population.sense.getNext()
             print photo_array_values
+            time.sleep(0.4)
             result = self.ann.propagate(photo_array_values)
             result = [int(round(x)) for x in result]
             result = ''.join(map(str, result))
