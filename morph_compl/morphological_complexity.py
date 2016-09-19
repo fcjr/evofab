32bdffde9ddf9252232ca0c3ec220c7f51b73920
John Peterson <sn0risn@gmail.com>
2016-09-19 18:41:43 -0400

    tries to do summation for shannon entropy (but zero-value angles break it)
    i converted to degrees and that is bad maybe?

--- morph_compl/morphological_complexity.py
+++ morph_compl/morphological_complexity.py
@@ -20,7 +20,7 @@ def calculate_perimeter(image):
 
     #sort them largest to smallest
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
-    print cnts[0]
+    #print cnts[0]
 
     #calculate the peremiter
     #if there are no visible contours in the image, return a fitness of 0
@@ -37,13 +37,41 @@ def calculate_perimeter(image):
     pts = [tuple(x[0]) for x in pts]
     for n in range(len(pts)-1):
         cv2.line(image, pts[n], pts[n+1], (0, 0, 0), 2, -1)
+    angles = []
     for n,pt in islice(enumerate(pts), 0, None, 2):
         a1, a2 = [a - b for a,b in zip(pts[n], pts[n-1])]
         b1, b2 = [a - b for a,b in zip(pts[n+1], pts[n])]
         dot = a1*b1 + a2*b2
         angle = acos(round(dot/(sqrt(a1*a1 + a2*a2) * sqrt(b1*b1 + b2*b2)), 10))
-        angle = angle * 180/pi
-        print n/2, "&", angle
+        angles.append(angle)
+        #print n/2, "&", angle
+    #bin_width = (max(angles) - min(angles))/10.0
+    #print "\n"*5, bin_width
+
+    #calculate PDF
+    ###############
+    bin_width = 7
+    num_bins = int(floor((max(angles) - min(angles))/bin_width))
+    PDF = []
+    for x in range(num_bins + 1):
+        l = min(angles) + (x * bin_width)
+        r = min(angles) + ((x+1) * bin_width)
+        num_in_range = 0
+        for angle in angles:
+            if angle > l and angle < r :
+                num_in_range += 1
+        PDF.append(num_in_range/float(len(angles)))
+    
+    for n,p in enumerate(PDF):
+        print (min(angles) + (n * bin_width)), "&", (min(angles) + ((n+1)*bin_width)), "&", p
+
+    #calculate entropy
+    ##################
+    e = 0
+    for p in PDF:
+        e += p * log(p, 10)
+    e = -e
+    print e
     #end
 
     color_interval = 255/len(cnts[0])
